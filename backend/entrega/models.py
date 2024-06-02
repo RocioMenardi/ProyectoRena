90f5c8b6357c4a77dd10fbfce6c78d66d160c907
RocioMenardi <romenardi08@gmail.com>
2024-06-02 18:53:44 -0300

    Backend
    Creacion del proyecto y modelos de entrega.

---
+++ backend/entrega/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+# Create your models here.
+class ModeloBase:
+    def borrado(self):
+        activo = False
+        self.activo = activo
+
+class Nafta(models.Model):
+    precio_litro = models.FloatField()
+    def __str__(self): #en el admin muestre el atributo (nombre) del objeto.
+        return f"{self.precio_litro}"
+
+class Cliente(models.Model, ModeloBase):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=10, blank=True)
+    direccion = models.CharField(max_length=50, blank=True)
+    descripcion = models.TextField(blank=True)
+    activo = models.BooleanField(default=True)
+    def __str__(self): #en el admin muestre el atributo (nombre) del objeto.
+        return f"{self.nombre} {self.apellido}"
+
+class Entrega(models.Model, ModeloBase):
+    fecha = models.DateField(auto_now_add=True) #para setear automaticamente la fecha y hora
+    hora = models.TimeField(auto_now_add=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name="entrega") #con related_name permite traer todas las entregas relacionadas a un cliente, teniendo el objeto cliente.
+    nafta = models.ForeignKey(Nafta, on_delete=models.SET_DEFAULT, default=0)
+    #usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="entregasUsuario")
+    activo = models.BooleanField(default=True)
+    
+    def __str__(self): #en el admin muestre el atributo (nombre) del objeto.
+            return f"{self.cliente.nombre} {self.cliente.apellido} {self.fecha}"
