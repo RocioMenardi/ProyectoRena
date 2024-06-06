70d2367e3d2dbb87957eed8bab33223f3e3f8aa8
RocioMenardi <romenardi08@gmail.com>
2024-06-06 14:36:17 -0300

    backend
    ABM de TipoProducto, lo probé en postman y funciona el get, post, put y delete

--- backend/producto/urls.py
+++ backend/producto/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import LitroAbm
+from .views import LitroAbm, TipoProductoAbm
 
 urlpatterns = [
     path('litro/', LitroAbm.as_view(), name="Litro"),
+    path('tipoProducto/', TipoProductoAbm.as_view(), name="TipoProducto"),
 ]
--- backend/producto/views.py
+++ backend/producto/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import Litro
+from .models import Litro, TipoProducto
 from rest_framework.response import Response
 
 
@@ -54,3 +54,80 @@ class LitroAbm (APIView):
         
         litro.delete()
         return Response({"Mensaje": "Litro se ha borrado con exito"})
+    
+class TipoProductoAbm(APIView):
+    def get(self,request):
+        tipos = TipoProducto.objects.all() #obtengo todos los objetos tipo producto
+        data = [] #necesito recorrerlos, creo una lista para almacenar los datos 
+
+        for tipo in tipos: #Recorrer cada tipo de producto
+            #Agregar los datos a la lista
+            data.append({
+                "id": tipo.id,
+                "nombre": tipo.nombre,
+                "descripcion": tipo.descripcion
+            }
+            )
+        return Response({"TiposProducto": data}) #retorno la respuesta en json
+    
+    def post(self,request): #crear
+        #Verificar que los campos nombre y descripcion estén presentes en la solicitud request.data
+        if not "nombre" in request.data or not "descripcion" in request.data: 
+            return Response({"Error":"Falta completar los campos nombre o descripcion"}, status=400)
+            #Si alguno de los campos falta, retorna una respuesta de error y el estado HTTP 400 (Bad Request).
+
+        #Obtener los valores de nombre y descripcion desde la solicitud
+        nombre = request.data.get("nombre")
+        descripcion = request.data.get("descripcion")
+
+        # Verificar si ya existe un TipoProducto con el mismo nombre
+        if not TipoProducto.objects.filter(nombre=nombre).exists():
+            tipo = TipoProducto.objects.create(nombre=nombre, descripcion=descripcion)
+            return Response({"Mensaje": "Nuevo tipo producto creado", "id":tipo.id}, status=200)
+            #Si no existe, creo un nuevo TipoProducto.
+        else: #Si ya existe el TipoProducto
+            return Response({"Error": "Ya existe un tipo producto con ese nombre"}, status=400)
+    
+    def put(self,request): #Modificar
+        #Verificar campo obligatorio id
+        if not "id" in request.data:
+            return Response({"Error": "Falta el id"}, status=400)
+        
+        #Obtener valor desde la solicitud
+        id = request.data.get("id")
+
+        #Intentar obtener el TipoProducto correspondiente
+        try:
+            tipo = TipoProducto.objects.get(id=id)
+        except TipoProducto.DoesNotExist:
+            return Response({"Error": "No existe el tipo producto"}, status=404)
+        
+        #Actualizar los campos proporcionados
+        if "nombre" in request.data:
+            tipo.nombre = request.data.get("nombre")
+        if "descripcion" in request.data:
+            tipo.descripcion = request.data.get("descripcion")
+
+        #Guardar los cambios en la base de datos
+        tipo.save()
+
+        #Retornar respuesta de éxito
+        return Response({"Mensaje": "Se modificó con éxito"}, status=200)
+
+    def delete(self,request):
+        #Verificar que el campo id esté en la solicitud
+        if not "id" in request.data:
+            return Response({"Error": "Falta el id"}, status=400)
+        
+        #Obtener el valor de id desde la solicitud
+        id = request.data.get("id")
+
+        #Intentar obtener el TipoProducto correspondiente al id proporcionado
+        try:
+            tipo = TipoProducto.objects.get(id=id)
+        except TipoProducto.DoesNotExist:
+            return Response({"Error": "No existe el tipo producto"}, status=404)
+        
+        #Eliminar el TipoProducto de la base de datos
+        tipo.delete()
+        return Response({"Se borró con éxito el tipo producto"}, status=200)
