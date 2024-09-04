a592b96d3fc28c667bddc1e069b35abecb7ba6b9
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2024-09-04 17:45:16 -0300

    probando conecxion

--- frontend/front/src/componentes/VerEntregas/VerEntrega.js
+++ frontend/front/src/componentes/VerEntregas/VerEntrega.js
@@ -1,52 +1,39 @@
 import "./VerEntrega.css";
+import React, { useState, useEffect } from 'react';
 
 const VerEntrega =() =>{
+    const [entregas, setEntregas] =  useState([]); // Estado para almacenar las entregas
+
+    useEffect(() => {
+    fetch("http://127.0.0.1:8000/entrega/entrega/")
+    .then(res=>res.json())
+    .then(respuesta => {
+        console.log(respuesta); // Verifica la respuesta aquí
+        setEntregas(respuesta.entregas || []);
+    })
+    .catch(error=> console.error("error", error))
+    
+},[])
+
 
     return(
         <div className="container">
             <div className="contGeneralInfo">
                 <h1>Entregas</h1>
+                
 
-                <div className="contInfo">
-                    <ul>
-                        <li><strong>Fecha:</strong> 12/02/8</li>
-                        <li><strong>Hora:</strong> 13:45</li>
-                        <li><strong>Cliente:</strong> rami berruezo</li>
-                        <li><strong>Nafta (Precio por Litro):</strong> 12000</li>
-                        <li><strong>Usuario:</strong> rena</li>
-                    </ul>
-                </div>
-
-                <div className="contInfo">
-                    <ul>
-                        <li><strong>Fecha:</strong> 12/02/8</li>
-                        <li><strong>Hora:</strong> 13:45</li>
-                        <li><strong>Cliente:</strong> rami berruezo</li>
-                        <li><strong>Nafta (Precio por Litro):</strong> 1200</li>
-                        <li><strong>Usuario:</strong> rena</li>
-                    </ul>
-                </div>
-                <div className="contInfo">
-                    <ul>
-                        <li><strong>Fecha:</strong> 12/02/8</li>
-                        <li><strong>Hora:</strong> 13:45</li>
-                        <li><strong>Cliente:</strong> rami berruezo</li>
-                        <li><strong>Nafta (Precio por Litro):</strong> 1200</li>
-                        <li><strong>Usuario:</strong> rena</li>
-                    </ul>
-                </div>
-                
-                <div className="contInfo">
-                    <ul>
-                        <li><strong>Fecha:</strong> 12/02/8</li>
-                        <li><strong>Hora:</strong> 13:45</li>
-                        <li><strong>Cliente:</strong> rami berruezo</li>
-                        <li><strong>Nafta (Precio por Litro):</strong> 1200</li>
-                        <li><strong>Usuario:</strong> rena</li>
-                    </ul>
-                </div>
-                
-                
+               {entregas.map((entrega, index) => (
+                    <div className="contInfo" key={index}>
+                        <ul>
+                            <li><strong>Fecha:</strong> {entrega.fecha}</li>
+                            <li><strong>Hora:</strong> {entrega.hora}</li>
+                            <li><strong>Cliente:</strong> {entrega.cliente}</li>
+                            <li><strong>Nafta (Precio por Litro):</strong> {entrega.nafta}</li>
+                            <li><strong>Usuario:</strong> {entrega.usuario}</li>
+                        </ul>
+                    </div>
+               ))}
+            
             </div> 
         </div>
     )
