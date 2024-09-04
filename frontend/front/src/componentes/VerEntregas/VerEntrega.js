b842b4f857fab9dcef7d9480f5a67a68796f5fee
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2024-09-04 13:51:45 -0300

    creacion de la pantalla Ver entregas

--- frontend/front/src/componentes/VerEntregas/VerEntrega.js
+++ frontend/front/src/componentes/VerEntregas/VerEntrega.js
@@ -1,10 +1,53 @@
-import style from "./VerEntrega";
+import "./VerEntrega.css";
 
 const VerEntrega =() =>{
 
     return(
-        <div className={style.container}>
-            hola
+        <div className="container">
+            <div className="contGeneralInfo">
+                <h1>Entregas</h1>
+
+                <div className="contInfo">
+                    <ul>
+                        <li><strong>Fecha:</strong> 12/02/8</li>
+                        <li><strong>Hora:</strong> 13:45</li>
+                        <li><strong>Cliente:</strong> rami berruezo</li>
+                        <li><strong>Nafta (Precio por Litro):</strong> 12000</li>
+                        <li><strong>Usuario:</strong> rena</li>
+                    </ul>
+                </div>
+
+                <div className="contInfo">
+                    <ul>
+                        <li><strong>Fecha:</strong> 12/02/8</li>
+                        <li><strong>Hora:</strong> 13:45</li>
+                        <li><strong>Cliente:</strong> rami berruezo</li>
+                        <li><strong>Nafta (Precio por Litro):</strong> 1200</li>
+                        <li><strong>Usuario:</strong> rena</li>
+                    </ul>
+                </div>
+                <div className="contInfo">
+                    <ul>
+                        <li><strong>Fecha:</strong> 12/02/8</li>
+                        <li><strong>Hora:</strong> 13:45</li>
+                        <li><strong>Cliente:</strong> rami berruezo</li>
+                        <li><strong>Nafta (Precio por Litro):</strong> 1200</li>
+                        <li><strong>Usuario:</strong> rena</li>
+                    </ul>
+                </div>
+                
+                <div className="contInfo">
+                    <ul>
+                        <li><strong>Fecha:</strong> 12/02/8</li>
+                        <li><strong>Hora:</strong> 13:45</li>
+                        <li><strong>Cliente:</strong> rami berruezo</li>
+                        <li><strong>Nafta (Precio por Litro):</strong> 1200</li>
+                        <li><strong>Usuario:</strong> rena</li>
+                    </ul>
+                </div>
+                
+                
+            </div> 
         </div>
     )
 };
