2a4c7afd24804f159587581305116da6b3cf9a15
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2024-09-17 16:59:49 -0300

    agregue el buscador en productos

--- frontend/front/src/componentes/Producto/verProductos/VerProductos.js
+++ frontend/front/src/componentes/Producto/verProductos/VerProductos.js
@@ -1,12 +1,16 @@
 import React,{useEffect, useState} from 'react'
 import './verProductos.css'
 import { useNavigate } from 'react-router-dom';
+import Buscador from '../../buscador/buscador';
 
 
 const VerProductos = () => {
 
     const [productos, setProductos] =  useState([]); // Estado para almacenar las entregas
     const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredProductos, setFilteredProductos] = useState([]);
+    
 
     const isMobileDevice = () => {
         return /Mobi|Android/i.test(navigator.userAgent) || window.innerWidth <= 768;
@@ -17,12 +21,24 @@ const VerProductos = () => {
     .then(res=>res.json())
     .then(respuesta => {
         setProductos(respuesta.Productos);
+        setFilteredProductos(respuesta.Productos);
         console.log(respuesta)
     })
     .catch(error=> console.error("error", error))
-    
-},[])
+    },[])
 
+    
+    useEffect(() => {
+        // Filtrar los libros en función del término de búsqueda
+        setFilteredProductos(productos.filter((item) =>
+          item.tipoProducto.toLowerCase().includes(searchTerm.toLowerCase())
+        ));
+      }, [searchTerm]);
+    
+      const handleSearchChange = (term) => {
+        setSearchTerm(term);
+      };
+    
     return(
         <div className="container">
 
@@ -31,8 +47,10 @@ const VerProductos = () => {
                 <button className="back-buttonNafta" onClick={() => navigate('/home')}>←</button> 
 
                 {isMobileDevice() && <button onClick={() => navigate('/agregarProducto')} className='buttonEntrega'>Registrar Producto</button>}
-                    <h1>Productos</h1>
-                {productos.map((producto, index) => (
+
+                <h1>Productos</h1>
+                <Buscador onSearch={handleSearchChange} />
+                {filteredProductos.map((producto, index) => (
                         <div className="contProducto" key={index}>
 
                             <p>{producto.tipoProducto}  {producto.litro}L</p>
---
+++ frontend/front/src/componentes/buscador/buscador.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import style from './buscador.module.css'; // Asumiendo que tienes un archivo CSS con los estilos
+
+const Buscador = ({ onSearch }) => {
+    // Estado para almacenar el valor del campo de búsqueda
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isClicked, setIsClicked] = useState(false);
+
+    // Efecto para llamar a onSearch cuando searchTerm cambia
+    useEffect(() => {
+        if (onSearch) {
+            onSearch(searchTerm);
+        }
+    }, [searchTerm, onSearch]);
+
+    const handleSearchClick = () => {
+        setIsClicked(true); // Marcar que se hizo clic
+    };
+
+    const handleBlur = () => {
+        if (searchTerm === '') {
+            setIsClicked(false);
+        }
+    };
+
+    return (
+        <>
+            <input
+                type="text"
+                placeholder="Buscar"
+                value={searchTerm}
+                onClick={handleSearchClick}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onBlur={handleBlur}
+                className={isClicked ? style.clicked : style.barra} // Cambia de estilo cuando se clickea    
+            />
+        </>
+    );
+};
+
+export default Buscador;
