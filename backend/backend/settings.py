97fcd4799a7664c64370e3c961138593fe0509a6
B3RZVS <ramiroberruezo10@gmail.com>
2024-09-21 20:22:12 -0300

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,15 +30,15 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
-    'http://192.168.68.51:8000',
-    'http://192.168.68.51:3000',
+    'http://192.168.0.89:8000',
+    'http://192.168.0.89:3000',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
-    'http://192.168.68.51:8000',
-    'http://192.168.68.51:3000',
+    'http://192.168.0.89:8000',
+    'http://192.168.0.89:3000',
 ]
 CORS_ALLOW_ALL_HEADERS=True
 CORS_ALLOW_CREDENTIALS = True
